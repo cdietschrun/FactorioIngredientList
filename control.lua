235ab3af42b2108d0dbf4d1d20e08acfb26e4533
Chris Dietsch <cdietschrunfast@gmail.com>
2025-02-23 00:18:20 -0500

    done chapter 6

--- control.lua
+++ control.lua
@@ -1,4 +1,46 @@
-local item_sprites = {"inserter", "transport-belt", "stone-furnace", "assembling-machine-3", "logistic-chest-storage", "sulfur", "utility-science-pack", "laser-turret"}
+local item_sprites = {"inserter", "transport-belt", "stone-furnace", "assembling-machine-3", "storage-chest", "sulfur", "utility-science-pack", "laser-turret"}
+
+local function build_sprite_buttons(player)
+    local player_storage = storage.players[player.index]
+
+    local button_table = player.gui.screen.il_main_frame.content_frame.button_frame.button_table
+    button_table.clear()
+
+    local number_of_buttons = player_storage.button_count
+    for i = 1, number_of_buttons do
+        local sprite_name = item_sprites[i]
+        local button_style = (sprite_name == player_storage.selected_item) and "yellow_slot_button" or "slot_button"
+        button_table.add{type="sprite-button", sprite=("item/" .. sprite_name), tags={action="il_select_button", item_name=sprite_name}, style=button_style}
+    end
+end
+
+local function initialize_storage(player)
+    game.print("player index: "..player.index)
+    storage.players[player.index] = { controls_active = true, button_count = 0, selected_item = nil } 
+end
+
+local function build_interface(player)
+    local player_storage = storage.players[player.index]
+
+    local screen_element = player.gui.screen
+    local main_frame = screen_element.add{type="frame", name="il_main_frame", caption={"il.hello_world"}}
+    main_frame.style.size = {385, 165}
+    main_frame.auto_center = true
+
+    local content_frame = main_frame.add{type="frame", name="content_frame", direction="vertical", style="il_content_frame"}
+    local controls_flow = content_frame.add{type="flow", name="controls_flow", direction="horizontal", style="il_controls_flow"}
+
+    local button_caption = (player_storage.controls_active) and {"il.deactivate"} or {"il.activate"}
+    controls_flow.add{type="button", name="il_controls_toggle", caption=button_caption}
+
+    local initial_button_count = player_storage.button_count
+    controls_flow.add{type="slider", name="il_controls_slider", value=initial_button_count, minimum_value=0, maximum_value=#item_sprites, style="notched_slider", enabled=player_storage.controls_active}
+    controls_flow.add{type="textfield", name="il_controls_textfield", text=tostring(initial_button_count), numeric=true, allow_decimal=false, allow_negative=false, style="il_controls_textfield", enabled=player_storage.controls_active}
+
+    local button_frame = content_frame.add{type="frame", name="button_frame", direction="horizontal", style="il_deep_frame"}
+    button_frame.add{type="table", name="button_table", column_count=#item_sprites, style="filter_slot_table"}
+    build_sprite_buttons(player)
+end
 
 -- Make sure the intro cinematic of freeplay doesn't play every time we restart
 -- This is just for convenience, don't worry if you don't understand how this works
@@ -10,25 +52,18 @@ script.on_init(function()
     end
 
     storage.players = {}
+
+    for _, player in pairs(game.players) do
+        initialize_storage(player)
+        build_interface(player)
+    end
 end)
 
 script.on_event(defines.events.on_player_created, function (event)
     local player = game.get_player(event.player_index)
-    
-    game.print("player index: "..player.index)
-    storage.players[player.index] = { controls_active = true, button_count = 0 } 
 
-    local screen_element = player.gui.screen
-    local main_frame = screen_element.add{type="frame", name="il_main_frame", caption={"il.hello_world"}}
-    main_frame.style.size = {385, 165}
-    main_frame.auto_center = true
-
-    local content_frame = main_frame.add{type="frame", name="content_frame", direction="vertical", style="il_content_frame"}
-    local controls_flow = content_frame.add{type="flow", name="controls_flow", direction="horizontal", style="il_controls_flow"}
-
-    controls_flow.add{type="button", name="il_controls_toggle", caption={"il.deactivate"}}
-    controls_flow.add{type="slider", name="il_controls_slider", value=0, minimum_value=0, maximum_value=#item_sprites, style="notched_slider"}
-    controls_flow.add{type="textfield", name="il_controls_textfield", text="0", numeric=true, allow_decimal=false, allow_negative=false, style="il_controls_textfield"}
+    initialize_storage(player)
+    build_interface(player)
 end)
 
 script.on_event(defines.events.on_gui_click, function (event)
@@ -43,6 +78,16 @@ script.on_event(defines.events.on_gui_click, function (event)
         local controls_flow = player.gui.screen.il_main_frame.content_frame.controls_flow
         controls_flow.il_controls_slider.enabled = player_storage.controls_active
         controls_flow.il_controls_textfield.enabled = player_storage.controls_active
+
+        build_sprite_buttons(player)
+    elseif event.element.tags.action == "il_select_button" then
+        local player = game.get_player(event.player_index)
+        local player_storage = storage.players[player.index]
+
+        local clicked_item_name = event.element.tags.item_name
+        player_storage.selected_item = clicked_item_name
+
+        build_sprite_buttons(player)
     end
 end)
 
@@ -56,6 +101,8 @@ script.on_event(defines.events.on_gui_value_changed, function(event)
 
         local controls_flow = player.gui.screen.il_main_frame.content_frame.controls_flow
         controls_flow.il_controls_textfield.text = tostring(new_button_count)
+
+        build_sprite_buttons(player)
     end
 end)
 
@@ -71,4 +118,8 @@ script.on_event(defines.events.on_gui_text_changed, function(event)
         local controls_flow = player.gui.screen.il_main_frame.content_frame.controls_flow
         controls_flow.il_controls_slider.slider_value = capped_button_count
     end
+end)
+
+script.on_event(defines.events.on_player_removed, function (event)
+    storage.players[event.player_index] = nil
 end)
